df2aef5f9565e37fdfb0f114f4237e001ccb43b9
Alexandra Valdes <alexandra.s.valdes@gmail.com>
2023-05-02 20:20:30 -0400

    add import file upload

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import datetime
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     test = 1;
     return render_template('index.html')
 
+
 @app.route('/import')
-def import_statement():
+def import_statement_page():
     return render_template('import.html')
 
+
+@app.route('/deposit')
+def deposit():
+    return render_template('deposit.html')
+
+
+@app.route('/monthly')
+def monthly_breakdown():
+    return render_template('monthly.html')
+
+
+@app.route('/import_file', methods=['POST'])
+def import_statement():
+    file = request.files['statement']
+    if file.filename != '':
+        file_path = r'statements\\' + file.filename
+        new_file = open(file_path, 'x')
+
+        for line in file:
+            sanitized_line = str(line)
+            # remove b'
+            sanitized_line = sanitized_line[2:]
+            # remove ,\n'
+            sanitized_line = sanitized_line[:-4]
+
+            new_file.write(sanitized_line)
+            new_file.write('\n')
+
+        new_file.close()
+
+    return redirect(url_for('index'))
---
+++ static/js/last_month_comparison_bar.js
@@ -0,0 +1,28 @@
+google.charts.load('current', {'packages':['bar']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+var data = google.visualization.arrayToDataTable([
+  ['Category', 'Last Month', 'This Month'],
+  ['Rent', 1000, 400],
+  ['Utilities', 1170, 460],
+  ['Groceries', 660, 1120],
+  ['Travel', 1030, 540],
+  ['Food', 100, 100],
+  ['Ian Savings', 100, 100],
+  ['Misc.', 100, 100]
+]);
+
+var options = {
+  chart: {
+    title: 'Last Month Comparison',
+  },
+  height: 200,
+  colors: ['#214E34', '#E09891']
+};
+
+var chart = new google.charts.Bar(document.getElementById('last_month_comparison_bar'));
+
+chart.draw(data, google.charts.Bar.convertOptions(options));
+}
+
